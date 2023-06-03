73fe3120eafa25aa2df40559afbd70a8e160c09d
unknown <sumittomar6636@gmail.com>
2023-06-03 12:53:33 +0530

    Adding file for test purposes only.

---
+++ src/SinglePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SinglePage() {
+  return (
+    <div>
+      <h1>Welcome to My Single Page!</h1>
+      {/* Add your content here */}
+    </div>
+  );
+}
+
+export default SinglePage;
